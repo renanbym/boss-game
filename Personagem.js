cf6f54fa7c7001a30e25275fa87e79b4cdd1f9be
Renan Mariano <renanbym@gmail.com>
2019-08-22 12:41:28 +0100

    play

--- Personagem.js
+++ Personagem.js
@@ -8,24 +8,54 @@ class Personagem {
     image
     line = 0
     column = 0
+    velocity
+    lastTime = new Date().getTime();
 
     constructor() {
 
-        this.params(arguments[0]);
+        this.params(arguments[0])
     }
 
-    params = ({ x, y, width, height, context, image }) => {
+    params = ({ x, y, width, height, context, image, velocity }) => {
         this.x = x
         this.y = y
         this.width = width
         this.height = height
         this.context = context
         this.image = image
+        this.velocity = velocity
+    }
+
+    nextFrame = () => {
+        const now = new Date().getTime();
+
+        if (this.lastPosition == this.SETA_BAIXO) {
+            this.line = 0
+        }
+
+        if (this.lastPosition == this.SETA_CIMA) {
+            this.line = 3
+        }
+
+        if (this.lastPosition == this.SETA_DIREITA) {
+            this.line = 2
+        }
+
+        if (this.lastPosition == this.SETA_ESQUERDA) {
+            this.line = 1
+        }
+
+        if (!this.lastTime) this.lastTime = now;
+
+        if (now - this.lastTime < this.intervalo) return;
+
+        this.column = this.column >= 2 ? 0 : ++this.column;
+
+        this.lastTime = now;
     }
 
     render = () => {
-
-        this.context.drawImage(this.image, this.line * 45, this.column * 45, 45, 45, this.x, this.y, this.width, this.height);
+        this.context.drawImage(this.image, this.column * 49, this.line * 48, 49, 48, this.x, this.y, this.width, this.height)
     }
 
     explode = () => {
